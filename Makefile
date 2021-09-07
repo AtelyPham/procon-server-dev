3fb3100c6095b8ee994b7059265b009aebc6f97b
AtelyPham <tinpham1412000@gmail.com>
2021-09-07 23:46:11 +0700

    Change file to test route

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ all: ;clean build
 
 build: ;yarn tsc:webpack
 
-clean: ;rm -rf build dist public deploy.zip
+clean: ;rm -rf build dist public *.zip
 
 run: ;docker run --rm --name be -p 443:433 -d procon-server:0.0
 
--- src/v1/routes/index.ts
+++ src/v1/routes/index.ts
@@ -1,14 +1,26 @@
-import { Router } from "express"
+import { Router, Request, Response } from "express"
 import matchRoute from "./match.route"
 import userRoute from "./user.route"
 import { setup, serve } from "swagger-ui-express"
 import SwaggerDocs from "../../../AtelyPham-Procon-Admin-API-1.0.0-resolved.json"
+import genResObj from "../helpers/genResObj"
 
 const router = Router()
 
+router.get("/v1", (req: Request, res: Response) => {
+  res.send(
+    genResObj(null, {
+      version: "1",
+      author: "Trung-Tin Pham",
+      description: "API for Procon Server App",
+    })
+  )
+})
+
 router.use("/v1/matches", matchRoute)
 router.use("/v1/users", userRoute)
-router.use("/v1/api-docs", serve)
-router.get("/v1/api-docs", setup(SwaggerDocs))
+
+router.use("/v1/docs", serve)
+router.get("/v1/docs", setup(SwaggerDocs))
 
 export default router
